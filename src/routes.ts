9707c5421827a8c1c652ba22cf7030988720fffa
syg0629 <syg0629@hanmail.net>
2025-05-18 23:57:13 +0900

    feat: 라우팅 구조 및 타입 정의 추가

---
+++ src/routes.ts
@@ -0,0 +1,37 @@
+export const routePaths = ["/", "/accordion"] as const;
+export type ROUTE_PATH = (typeof routePaths)[number];
+
+type BaseRoute = {
+  key: ROUTE_PATH;
+  link: ROUTE_PATH;
+  name: string;
+};
+export type ParentRoute = BaseRoute & {
+  children: ROUTE_PATH[];
+};
+export type ChildRoute = BaseRoute & {
+  children: ((props: unknown) => JSX.Element) | null;
+};
+export type ROUTE = ParentRoute | ChildRoute;
+
+export const routes: Record<ROUTE_PATH, ROUTE> = {
+  "/": {
+    key: "/",
+    link: "/",
+    name: "root",
+    children: ["/accordion"],
+  },
+  "/accordion": {
+    key: "/accordion",
+    link: "/accordion",
+    name: "01. 아코디언",
+    children: null,
+  },
+};
+
+export const isParentRoute = (route: ROUTE): route is ParentRoute =>
+  Array.isArray(route.children);
+
+export const gnbRootList = (routes["/"] as ParentRoute).children.map(
+  (r) => routes[r]
+);
