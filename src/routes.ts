372ef886197c89e2fe8b221eab31d9365290ad1c
syg0629 <syg0629@hanmail.net>
2025-05-19 23:57:36 +0900

    feat: 라우팅 경로 및 컴포넌트 추가

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,15 @@
-export const routePaths = ["/", "/accordion"] as const;
+import Test1 from "./components/test1";
+import Test2_React from "./components/test2/react";
+import Test2_Vanilla from "./components/test2/vanilla";
+
+// export const routePaths = ["/", "/accordion"] as const;
+export const routePaths = [
+  "/",
+  "/test1",
+  "/test2",
+  "/test2/vanilla",
+  "/test2/react",
+] as const;
 export type ROUTE_PATH = (typeof routePaths)[number];
 
 type BaseRoute = {
@@ -15,17 +26,47 @@ export type ChildRoute = BaseRoute & {
 export type ROUTE = ParentRoute | ChildRoute;
 
 export const routes: Record<ROUTE_PATH, ROUTE> = {
+  // "/": {
+  //   key: "/",
+  //   link: "/",
+  //   name: "root",
+  //   children: ["/accordion"],
+  // },
+  // "/accordion": {
+  //   key: "/accordion",
+  //   link: "/accordion",
+  //   name: "01. 아코디언",
+  //   children: null,
+  // },
   "/": {
     key: "/",
     link: "/",
     name: "root",
-    children: ["/accordion"],
+    children: ["/test1", "/test2"],
   },
-  "/accordion": {
-    key: "/accordion",
-    link: "/accordion",
-    name: "01. 아코디언",
-    children: null,
+  "/test1": {
+    key: "/test1",
+    link: "/test1",
+    name: "테스트1",
+    children: Test1,
+  },
+  "/test2": {
+    key: "/test2",
+    link: "/test2/vanilla",
+    name: "테스트2",
+    children: ["/test2/vanilla", "/test2/react"],
+  },
+  "/test2/vanilla": {
+    key: "/test2/vanilla",
+    link: "/test2/vanilla",
+    name: "Vanilla",
+    children: Test2_Vanilla,
+  },
+  "/test2/react": {
+    key: "/test2/react",
+    link: "/test2/react",
+    name: "React",
+    children: Test2_React,
   },
 };
 
