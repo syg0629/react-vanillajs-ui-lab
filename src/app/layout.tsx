4e6a6a06ec7474b3fd6d1287753a11f242ce257d
syg0629 <syg0629@hanmail.net>
2025-05-19 23:58:25 +0900

    feat: Gnb 컴포넌트 추가 및 레이아웃에 통합

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
+import Gnb from "./gnb";
 
 export const metadata: Metadata = {
   title: "UI요소모음 | FE재남",
@@ -10,6 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <body>
+        <Gnb />
         <main>{children}</main>
       </body>
     </html>
